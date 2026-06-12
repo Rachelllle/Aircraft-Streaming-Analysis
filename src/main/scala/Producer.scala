ddb72619b5851ed255ae1fa161af17e275f21939
Nassilya <belguedjnassilya@gmail.com>
2026-06-12 17:37:29 +0200

    update 2 Producer

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -3,13 +3,16 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.IOUtils
 import org.apache.spark.util.SerializableConfiguration
 
+
 object Producer {
   def main(args: Array[String]): Unit = {
     val inputPath      = "data/input"
     val outputPath     = "data/output"
     val nbPhotos       = 20      // nb photos par batch
-    val batchDuration  = 5       // cadence en sec
-    val isLoop         = true
+
+    // Option -> copy par défaut si non move
+    val mode = if (args.length > 0) args(0) else "copy"
+    val moveFiles = mode.equalsIgnoreCase("move")
 
     val conf = new SparkConf()
       .setAppName("ImageProducer")
@@ -17,35 +20,32 @@ object Producer {
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
 
+    val rdd = sc.binaryFiles(inputPath)
+    val batch = rdd.take(nbPhotos)
+
     val confSer = new SerializableConfiguration(sc.hadoopConfiguration)
 
-    while (isLoop) {
-      val rdd = sc.binaryFiles(inputPath)
-
-      rdd.coalesce(1).foreachPartition { partitionIterator =>
-        val fs = FileSystem.get(confSer.value)
-
-        partitionIterator.grouped(nbPhotos).foreach { batch =>
-          batch.foreach { case (path, content) =>
-            val srcPath  = new Path(path)
-            val fileName = srcPath.getName
-            val outFile  = new Path(outputPath, fileName)
-            val in  = content.open()
-            val out = fs.create(outFile, true)
-            try {
-              IOUtils.copyBytes(in, out, confSer.value, false)
-            } finally {
-              in.close()
-              out.close()
-            }
-          }
-          println(s"Batch traité : ${batch.length} images écrites dans $outputPath")
-        }
+    batch.foreach { case (path, content) =>
+      val fs = FileSystem.get(confSer.value)
+      val srcPath  = new Path(path)
+      val fileName = srcPath.getName
+      val outFile  = new Path(outputPath, fileName)
+      val in  = content.open()
+      val out = fs.create(outFile, true)
+      try {
+        IOUtils.copyBytes(in, out, confSer.value, false)
+      } finally {
+        in.close()
+        out.close()
       }
 
-      Thread.sleep(batchDuration * 1000)
+      if (moveFiles) {
+        fs.delete(srcPath, false)
+      }
     }
 
+    val action = if (moveFiles) "Déplacement" else "Copie"
+    println(s"$action terminé.. vers : $outputPath (${batch.length} images)")
     sc.stop()
   }
 }
